439bae1fdf2c5f5d7577ad345f37c6cf5bbfd853
TomTheHypeEngine <thomasi.93@hotmail.de>
2017-01-06 23:19:22 +0100

    Add and adjust services directory from tutorial

---
+++ src/services/tweet-service.js
@@ -0,0 +1,70 @@
+import {inject} from 'aurelia-framework';
+import Fixtures from './fixtures';
+import {LoginStatus} from './messages';
+import {EventAggregator} from 'aurelia-event-aggregator';
+import AsyncHttpClient from './async-http-client';
+
+@inject(Fixtures, EventAggregator, AsyncHttpClient)
+export default class TweetService {
+
+  tweets = [];
+  users = [];
+
+  constructor(data, ea, ac) {
+    // this.methods = data.methods;
+    this.ea = ea;
+    this.ac = ac;
+  }
+
+  getUsers() {
+    this.ac.get('/api/users').then(res => {
+      this.users = res.content;
+    });
+  }
+
+  tweet(content) {
+    const tweet = {
+      content: content
+    };
+    this.ac.post('/api/tweets', tweet).then(res => {
+      const returnedTweets = res.content;
+      this.tweets.push(returnedTweets);
+      console.log(amount + ' donated to ' + candidate.firstName + ' ' + candidate.lastName + ': ' + method);
+      console.log('Tweet created with text ' + content);
+      this.ea.publish(new TimelineUpdate(this.tweets));
+    });
+  }
+
+  isAuthenticated() {
+    return this.ac.isAuthenticated();
+  }
+
+  register(firstName, lastName, email, password) {
+    const newUser = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password
+    };
+    this.ac.post('/api/users', newUser).then(res => {
+      this.getUsers();
+    });
+  }
+
+  login(email, password) {
+    const user = {
+      email: email,
+      password: password
+    };
+    this.ac.authenticate('/api/users/authenticate', user);
+  }
+
+  logout() {
+    const status = {
+      success: false,
+      message: ''
+    };
+    this.ac.clearAuthentication();
+    this.ea.publish(new LoginStatus(new LoginStatus(status)));
+  }
+}
