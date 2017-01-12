ef86846575c5d0eeb703098583323a0932810663
TomTheHypeEngine <thomasi.93@hotmail.de>
2017-01-12 16:29:18 +0100

    Added Admin panel ui (no functionality yet)

--- src/viewmodels/admin/tweet_administration.js
+++ src/viewmodels/admin/tweet_administration.js
@@ -1,12 +1,24 @@
 import {inject} from 'aurelia-framework';
 import TweetService from '../../services/tweet-service';
 import {EventAggregator} from 'aurelia-event-aggregator';
+import {TimelineUpdate} from '../../services/messages';
 
 @inject(TweetService, EventAggregator)
 export class TweetAdministration {
 
+  loggedInUser = null;
+  globalTweets = [];
+
   constructor(ts, ea) {
     this.ts = ts;
     this.ea = ea;
+    this.loggedInUser = this.ts.loggedInUser;
+    this.ea.subscribe(TimelineUpdate, res => {
+      this.globalTweets = res.tweets;
+    });
+  }
+
+  attached() {
+    this.ts.getTweets();
   }
 }
--- src/viewmodels/admin/user_administration.js
+++ src/viewmodels/admin/user_administration.js
@@ -1,12 +1,24 @@
 import {inject} from 'aurelia-framework';
 import TweetService from '../../services/tweet-service';
 import {EventAggregator} from 'aurelia-event-aggregator';
+import {UserUpdate} from '../../services/messages';
 
 @inject(TweetService, EventAggregator)
 export class UserAdministration {
 
+  userList = [];
+  loggedInUser = null;
+
   constructor(ts, ea) {
     this.ts = ts;
     this.ea = ea;
+    this.loggedInUser = this.ts.loggedInUser;
+    this.ea.subscribe(UserUpdate, res => {
+      this.userList = res.users;
+    });
+  }
+
+  attached() {
+    this.ts.getUsers();
   }
 }
