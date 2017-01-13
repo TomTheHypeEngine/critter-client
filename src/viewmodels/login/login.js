172d89b9023e47ae26593754180faade0c874e68
TomTheHypeEngine <thomasi.93@hotmail.de>
2017-01-13 23:35:52 +0100

    Remove pre entered values from login and signup

--- src/viewmodels/login/login.js
+++ src/viewmodels/login/login.js
@@ -4,8 +4,8 @@ import TweetService from '../../services/tweet-service';
 @inject(TweetService)
 export class Login {
 
-  email = 'marge@simpson.com';
-  password = 'secret';
+  email = '';
+  password = '';
 
   constructor(ts) {
     this.ts = ts;
